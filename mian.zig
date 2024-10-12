9f2d94191be1c138d9ec63a3a8e60a1a1c7191a3
MiguelCock <macockc@eafit.edu.co>
2024-10-12 18:49:29 -0500

    for tests

--- mian.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-
-pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
-    const a = [_]u8{ 'a', 'e' };
-    const e = a.len;
-    std.debug.print("{}", .{e});
-}
---
+++ test_fot.zig
@@ -0,0 +1,18 @@
+const expect = @import("std").testing.expect;
+
+test "for" {
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string, 0..) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string, 0..) |_, index| {
+        _ = index;
+    }
+}
